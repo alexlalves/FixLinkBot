fffc1c3cb3b45d9494919ce8386bf37200177d2b
Magmagan <alexlalves@live.com>
2023-05-26 16:32:48 -0300

    find broken URLs with urlextract

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
 import os
 
+from typing import Any, Union
+
 import praw
 
 from dotenv import load_dotenv
+from urlextract import URLExtract
+
+def is_broken_url(url: Union[str, Any]) -> bool:
+    if isinstance(url, str):
+        return (
+            url.find('\\_') != -1 \
+            or url.find('%5C') != -1
+        )
+    raise TypeError(f'Expected {url} to be of type str')
 
 load_dotenv()
 
@@ -14,10 +25,15 @@ reddit = praw.Reddit(
     username = os.environ['USERNAME'],
 )
 
-for submission in reddit.subreddit("test").hot(limit = 10):
-    print(submission.title)
-
-subreddit = reddit.subreddit("test")
+subreddit = reddit.subreddit('test')
 for comment in subreddit.stream.comments(skip_existing = False):
-    print(comment.body)
-    # print(dir(comment))
+    extractor = URLExtract()
+    urls = extractor.find_urls(
+        text = comment.body,
+        only_unique = True,
+    )
+
+    broken_urls = [url for url in urls if is_broken_url(url)]
+
+    if broken_urls:
+        print(broken_urls)
