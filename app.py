65efc2614609a491b07cedb201f9a9de25ec0e36
Magmagan <alexlalves@live.com>
2023-05-26 11:16:12 -0300

    start testing praw's functionality

--- app.py
+++ app.py
@@ -7,17 +7,17 @@ from dotenv import load_dotenv
 load_dotenv()
 
 reddit = praw.Reddit(
-  client_id = os.environ['CLIENT_ID'],
-  client_secret = os.environ['CLIENT_SECRET'],
-  password = os.environ['PASSWORD'],
-  user_agent = os.environ['USER_AGENT'],
-  username = os.environ['USERNAME'],
+    client_id = os.environ['CLIENT_ID'],
+    client_secret = os.environ['CLIENT_SECRET'],
+    password = os.environ['PASSWORD'],
+    user_agent = os.environ['USER_AGENT'],
+    username = os.environ['USERNAME'],
 )
 
 for submission in reddit.subreddit("test").hot(limit = 10):
-  print(submission.title)
+    print(submission.title)
 
 subreddit = reddit.subreddit("test")
 for comment in subreddit.stream.comments(skip_existing = False):
-  print(comment.body)
-  # print(dir(comment))
+    print(comment.body)
+    # print(dir(comment))
