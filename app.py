c5df713e22f3302f051e5866be836503aee08275
Magmagan <alexlalves@live.com>
2023-05-26 17:07:20 -0300

    move script code into main() to avoid variable name confusion

--- app.py
+++ app.py
@@ -16,35 +16,39 @@ def is_broken_url(url: Union[str, Any]) -> bool:
     raise TypeError(f'Expected {url} to be of type str')
 
 def has_replied_to_broken_url_comment(replies):
-    for comment1 in replies:
-        print(comment1.author)
-        if comment1.author == os.environ['USERNAME']:
+    for comment in replies:
+        print(comment.author)
+        if comment.author == os.environ['USERNAME']:
             return True
     return False
 
-load_dotenv()
+def main():
+    load_dotenv()
 
-reddit = praw.Reddit(
-    client_id = os.environ['CLIENT_ID'],
-    client_secret = os.environ['CLIENT_SECRET'],
-    password = os.environ['PASSWORD'],
-    user_agent = os.environ['USER_AGENT'],
-    username = os.environ['USERNAME'],
-)
-
-subreddit = reddit.subreddit('test')
-for comment in subreddit.stream.comments(skip_existing = False):
-    extractor = URLExtract()
-    urls = extractor.find_urls(
-        text = comment.body,
-        only_unique = True,
+    reddit = praw.Reddit(
+        client_id = os.environ['CLIENT_ID'],
+        client_secret = os.environ['CLIENT_SECRET'],
+        password = os.environ['PASSWORD'],
+        user_agent = os.environ['USER_AGENT'],
+        username = os.environ['USERNAME'],
     )
 
-    broken_urls = [url for url in urls if is_broken_url(url)]
+    subreddit = reddit.subreddit('test')
+    for comment in subreddit.stream.comments(skip_existing = False):
+        extractor = URLExtract()
+        urls = extractor.find_urls(
+            text = comment.body,
+            only_unique = True,
+        )
 
-    if broken_urls:
-        print(broken_urls)
-        print(comment.replies)
-        comment.refresh()
-        print(comment.replies.list())
-        print(has_replied_to_broken_url_comment(comment.replies))
+        broken_urls = [url for url in urls if is_broken_url(url)]
+
+        if broken_urls:
+            print(broken_urls)
+            print(comment.replies)
+            comment.refresh()
+            print(comment.replies.list())
+            print(has_replied_to_broken_url_comment(comment.replies))
+
+if __name__ == '__main__':
+    main()
